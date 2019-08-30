60b60ac60af536636279c4d2f5995950bb16425c
Gregg Midon <3g.sr.yo@gmail.com>
2019-08-30 07:30:42 -0700

    Initial commit.

---
+++ src/main/java/midon/bbx/bbxapi/entity/PlayerStatCompositeKey.java
@@ -0,0 +1,60 @@
+package midon.bbx.bbxapi.entity;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class PlayerStatCompositeKey implements Serializable {
+
+	private static final long serialVersionUID = 4039980315029292251L;
+
+	private String playerId;
+	private int season;
+	
+	public PlayerStatCompositeKey() {}
+	
+	public PlayerStatCompositeKey(String playerId, int season) {
+		this.playerId = playerId;
+		this.season = season;
+	}
+
+	public String getPlayerId() {
+		return playerId;
+	}
+
+	public void setPlayerId(String playerId) {
+		this.playerId = playerId;
+	}
+
+	public int getSeason() {
+		return season;
+	}
+
+	public void setSeason(int season) {
+		this.season = season;
+	}
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(playerId, season);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if(this == obj)
+			return true;
+
+		if(!(obj instanceof PlayerStatCompositeKey)) {
+	        return false;
+	    }
+		
+		PlayerStatCompositeKey other = (PlayerStatCompositeKey) obj;
+		if(!playerId.equals(other.playerId))
+			return false;
+		
+		if(season != other.season)
+			return false;
+		
+		return true;
+	}
+	
+}
