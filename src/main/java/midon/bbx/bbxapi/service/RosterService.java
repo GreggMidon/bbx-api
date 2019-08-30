bfd04f2eb9a61a8b63d204431a9ecfd126f3b244
Gregg Midon <3g.sr.yo@gmail.com>
2019-08-30 07:35:33 -0700

    Added @Autowired annotation.

--- src/main/java/midon/bbx/bbxapi/service/RosterService.java
+++ src/main/java/midon/bbx/bbxapi/service/RosterService.java
@@ -2,6 +2,7 @@ package midon.bbx.bbxapi.service;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import midon.bbx.bbxapi.entity.Player;
@@ -12,6 +13,7 @@ public class RosterService {
 	
 	private RosterRepository repository;
 	
+	@Autowired
 	public RosterService(RosterRepository repository) {
 		this.repository = repository;
 	}
