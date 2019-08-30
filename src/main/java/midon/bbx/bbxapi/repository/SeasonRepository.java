e2fd08d765417805d098889cd2c817395df0511a
Gregg Midon <3g.sr.yo@gmail.com>
2019-08-30 07:34:14 -0700

    Changed SQL for season list.

--- src/main/java/midon/bbx/bbxapi/repository/SeasonRepository.java
+++ src/main/java/midon/bbx/bbxapi/repository/SeasonRepository.java
@@ -17,24 +17,9 @@ public class SeasonRepository {
     
     @SuppressWarnings("unchecked")
     public List<Integer> getSeasons() {
-        String sql = "select distinct season from game_hdr";
+        String sql = "select distinct season from game_hdr order by season";
         Query query = entityManager.createNativeQuery(sql);
         return query.getResultList();
     }
-/*    
-    @SuppressWarnings("unchecked")
-    public List<Bar> getBar(String s, Long l) {
-        String sql = "select * from bar where s = ? and l = ?";
-        Query qry = entityManager.createNativeQuery(sql);
-        qry.setParameter(1, s);
-        qry.setParameter(2, l);
-        List<Object[]> objectArrays = qry.getResultList();
-        List<Bean> beans = new ArrayList<FooBean>();
-        for (Object[] objArr : objectArrays) {
-            beans.add(new Value(objArr[0], objArr[1], objArr[2], objArr[3], objArr[4]));
-        }
-        return beans;
-    }
-*/
 }
 
