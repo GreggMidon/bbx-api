211a74fc3e25fb2b81b12a949bed9e7ca137d93c
Gregg Midon <3g.sr.yo@gmail.com>
2019-08-30 07:30:15 -0700

    Added CORS annotation.

--- src/main/java/midon/bbx/bbxapi/controller/SeasonController.java
+++ src/main/java/midon/bbx/bbxapi/controller/SeasonController.java
@@ -6,6 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,6 +21,7 @@ public class SeasonController {
 	@Autowired
 	SeasonService service;
 
+	@CrossOrigin(origins = "http://localhost:4200")
 	@GetMapping("/seasons")
 	public ResponseEntity<List<Integer>> getSeasons() {
 		logger.debug("Request: /seasons");
